8d4e2182be4794964fd9771a16917d04ab574ae5
Lucky <elurilawrence@gmail.com>
2026-02-26 16:36:50 +0530

    after integrating with google

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import './App.css';
+import { BrowserRouter } from "react-router-dom";
 import ResumeMaker from './resume-maker';
+import GATracker from "./analytics/ga";
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
+      <GATracker />
       <ResumeMaker />
-    </>
+    </BrowserRouter>
+    // <>
+    //   <ResumeMaker />
+    // </>
 
   );
 }
---
+++ src/analytics/ga.js
@@ -0,0 +1,26 @@
+import React, { useEffect } from "react";
+import { useLocation } from "react-router-dom";
+import ReactGA from "react-ga4";
+
+const MEASUREMENT_ID = "G-CMQT1VF6XM";
+
+let inited = false;
+
+export default function GATracker() {
+  const location = useLocation();
+
+  useEffect(() => {
+    if (!inited) {
+      ReactGA.initialize(MEASUREMENT_ID);
+      inited = true;
+    }
+  }, []);
+
+  useEffect(() => {
+    if (!inited) return;
+    const page = location.pathname + location.search;
+    ReactGA.send({ hitType: "pageview", page });
+  }, [location]);
+
+  return null;
+}

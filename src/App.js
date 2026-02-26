e9b96f65dcdd99466c62ebc8c0dda3d2b28b1eac
Lucky <elurilawrence@gmail.com>
2026-02-26 12:18:20 +0530

    after failed

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
 import './App.css';
 import ResumeMaker from './resume-maker';
-import About from './resume-maker2';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<ResumeMaker />} />
-        <Route path="/about" element={<About />} />
-      </Routes>
-    </BrowserRouter>
+    <>
+      <ResumeMaker />
+    </>
+
   );
 }
 
